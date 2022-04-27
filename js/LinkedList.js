a33781a51de0bf36e039d2863695a952738464e2
Jaime Alvarez <jealvarez123@gmail.com>
2022-04-27 17:09:39 -0500

    added LinkedList file

---
+++ js/LinkedList.js
@@ -0,0 +1,52 @@
+const Node = require('./Node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    //this checks if tail has a value, if not create a node
+    if (!tail) {
+      this.head = new Node(data);
+    } else {
+      //cycle through until we find the end
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+
+  removeHead() {
+    const removeHead = this.head;
+    if (!removeHead) {
+      return;
+    }
+    this.head = removeHead.getNextNode();
+    return removeHead.data;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+}
+
+module.exports = LinkedList;
